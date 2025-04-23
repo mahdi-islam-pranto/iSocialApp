9afb3f5ad7e6b57efb12bec1e19d801f1b738ec3
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-04-23 19:23:10 +0600

    notification added for foreground and background API

--- lib/model/MenuItems.dart
+++ lib/model/MenuItems.dart
@@ -2,22 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'MenuItem.dart';
 
-class MenuItems{
-
+class MenuItems {
   static const itemProfile = DropMenuItem(
-    text : "Evalyevaly@gmail.com",
-    icon : Icons.person_outline_outlined
-  );
+      text: "Evalyevaly@gmail.com", icon: Icons.person_outline_outlined);
 
-  static const itemSignOut = DropMenuItem(
-      text : "Sign Out",
-      icon : Icons.logout
-  );
-
+  static const itemSignOut = DropMenuItem(text: "Sign Out", icon: Icons.logout);
 
   static const List<DropMenuItem> itemsFirst = [
     itemProfile,
     itemSignOut,
   ];
 }
-
